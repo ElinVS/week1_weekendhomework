d54960bdc988775e324000e9ece342428ba6559c
ElinVS <elinvsvennberg@gmail.com>
2022-04-02 12:43:42 +0100

    solution for q6

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -3,7 +3,6 @@
 def get_pet_shop_name(shop_name):
     return shop_name["name"]
 
-#get sum cash 1000
 def get_total_cash(sum_of_cash):
     return sum_of_cash["admin"]["total_cash"]
 
@@ -13,6 +12,9 @@ def add_or_remove_cash(pet_shop, number):
 
 def get_pets_sold(pet_shop):
     return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop,number):
+    pet_shop["admin"]["pets_sold"] += number
     
     
 
