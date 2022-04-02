5a7619fbf1b46b5e106d46c83ac043383cb64c99
ElinVS <elinvsvennberg@gmail.com>
2022-04-02 12:34:24 +0100

    solution for q 3&4

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,9 +1,32 @@
 # WRITE YOUR FUNCTIONS HERE
 
-#Return "camelot of pets"
 def get_pet_shop_name(shop_name):
     return shop_name["name"]
 
 #get sum cash 1000
 def get_total_cash(sum_of_cash):
     return sum_of_cash["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, number):
+    pet_shop["admin"]["total_cash"] += number
+    pet_shop["admin"]["total_cash"] + number
+    
+    
+
+
+
+
+
+
+
+
+   
+
+
+
+
+    
+
+
+
+
