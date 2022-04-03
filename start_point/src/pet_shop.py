514ccc486e3b1dd22b840917bd3ab6adb3d1541b
ElinVS <elinvsvennberg@gmail.com>
2022-04-03 10:13:44 +0100

    solution for q8

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -19,9 +19,34 @@ def increase_pets_sold(pet_shop,number):
 def get_stock_count(pet_shop):
     return len(pet_shop["pets"])
 
+#if i define a function that loops through the dictionary then i should get 
+def get_pets_by_breed(pet_shop,type_of_breed):    
+    list = []
+    path_to_breed = pet_shop["pets"]
+    for x in path_to_breed:
+        if x["breed"] == type_of_breed:
+            list.append(x["breed"])
+    return list
+     
 
 
+# def find_pet_by_name(pet_shop, pet_name):
+#         for pet in pet_shop["pets"]:
+#             if pet["name"] == pet_name:
+#                 return pet
 
+# def get_customer_cash(customer):
+#     return customer["cash"]     
+
+
+# def remove_customer_cash(person,number):
+#     person["cash"] -= number
+
+# # def get_customer_pet_count(customer):
+# #     customer["pets"].append(0)
+# #     for x in customer["pets"]:
+# #         if x == 0:
+# #             return customer["pets"]
 
 
 
