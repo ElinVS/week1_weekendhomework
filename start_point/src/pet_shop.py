c208654c48ed835acd4ebd228b6690a3981ddfa4
ElinVS <elinvsvennberg@gmail.com>
2022-04-03 16:44:24 +0100

    solution for q12

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -34,22 +34,44 @@ def find_pet_by_name(pet_shop,pet_name):
         if pet["name"] == pet_name:
             return pet
 
+# pet_name = "Arthur"
+# pet_shop = cc_pet_shop
+
+# def remove_pet_by_name(pet_shop,pet_name):
+#     path_to_name =  pet_shop["pets"]
+
+#     for pet in path_to_name:
+#         if pet["name"] == pet_name:
+#             path_to_name.pop(pet["name"])
+    
+ 
+def add_pet_to_stock(pet_shop, new_pet):
+    new_pet = {"name": "Billy",
+                    "pet_type": "dog",
+                    "breed": "Dachshund",
+                    "price": 2000,}
+    path_to_dict = pet_shop["pets"]
+
+    path_to_dict.append(new_pet)
+
+
+def get_customer_cash(customer):
+    return customer["cash"]     
+
+
+def remove_customer_cash(person,number):
+    person["cash"] -= number
+
+def get_customer_pet_count(customer):
+    customer["pets"].append(0)
+    for x in customer["pets"]:
+        if x == 0:
+            return x
+
+# def add_pet_to_customer(person,new_pet):
 
 
 
-
-# def get_customer_cash(customer):
-#     return customer["cash"]     
-
-
-# def remove_customer_cash(person,number):
-#     person["cash"] -= number
-
-# # def get_customer_pet_count(customer):
-# #     customer["pets"].append(0)
-# #     for x in customer["pets"]:
-# #         if x == 0:
-# #             return customer["pets"]
 
 
 
